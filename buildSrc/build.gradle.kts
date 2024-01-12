3749ccf2a4bbac69dea8ffe0e81567e2d01bfca2
zizoh <zizoh.anto@getir.com>
2024-01-12 15:44:47 +0100

    remove unused navigation safe args

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,15 +18,12 @@ object Plugin {
     object Version {
         const val kotlin: String = "1.9.20"
         const val androidGradle: String = "8.1.2"
-        const val navigation: String = "2.7.5"
         const val daggerHiltAndroid: String = "2.48.1"
         const val spotless: String = "4.0.1"
     }
 
     const val kotlin: String = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}"
     const val androidGradle: String = "com.android.tools.build:gradle:${Version.androidGradle}"
-    const val navigationSafeArgs: String =
-        "androidx.navigation:navigation-safe-args-gradle-plugin:${Version.navigation}"
     const val daggerHilt: String =
         "com.google.dagger:hilt-android-gradle-plugin:${Version.daggerHiltAndroid}"
     const val spotless: String = "com.diffplug.spotless:spotless-plugin-gradle:${Version.spotless}"
@@ -35,7 +32,6 @@ object Plugin {
 dependencies {
     implementation(Plugin.kotlin)
     implementation(Plugin.androidGradle)
-    implementation(Plugin.navigationSafeArgs)
     implementation(Plugin.daggerHilt)
     implementation(Plugin.spotless)
 }
