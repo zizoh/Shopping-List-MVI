d3e35fc90c7e47ddfa14bf59da1ca7e7e227180e
Zizoh Anto <zizohanto@hotmail.com>
2024-02-01 15:24:49 +0100

    replace joda time with kotlinx-datetime (#36)
    Co-authored-by: zizoh <zizoh.anto@getir.com>

--- app/src/main/java/com/zizohanto/android/tobuy/ApplicationClass.kt
+++ app/src/main/java/com/zizohanto/android/tobuy/ApplicationClass.kt
@@ -2,13 +2,6 @@ package com.zizohanto.android.tobuy
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import net.danlew.android.joda.JodaTimeAndroid
 
 @HiltAndroidApp
-class ApplicationClass : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        JodaTimeAndroid.init(this)
-    }
-}
+class ApplicationClass : Application()
