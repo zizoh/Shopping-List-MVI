6107150dca4bf4fb0be3194568e95ddf2407f6d7
zizoh <zizoh.anto@getir.com>
2024-03-26 18:43:12 +0100

    undo configure dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,3 @@
 rootProject.name = "ShoppingList"
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 include(":app")
 include(":shared")
